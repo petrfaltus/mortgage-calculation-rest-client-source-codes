de1e1fa1ee1665a59a93171b5679423bb6040fce
Petr Faltus <petrfaltus@petrfaltus.net>
2021-01-29 09:53:47 +0100

    Java/Maven - create the empty console application

---
+++ java-maven/src/main/java/cz/petrfaltus/mortgage_calc_restclient/Program.java
@@ -0,0 +1,11 @@
+package cz.petrfaltus.mortgage_calc_restclient;
+
+import static java.lang.System.out;
+
+public class Program {
+
+    public static void main(String[] args) {
+        out.println("YES");
+    }
+
+}
