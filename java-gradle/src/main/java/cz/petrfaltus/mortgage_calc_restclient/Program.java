213599fbb32f7eb168bfc7b75b0765d43b04a844
Petr Faltus <petrfaltus@petrfaltus.net>
2021-01-29 12:28:46 +0100

    Java/Gradle - implement the web POST request for calculation

--- java-gradle/src/main/java/cz/petrfaltus/mortgage_calc_restclient/Program.java
+++ java-gradle/src/main/java/cz/petrfaltus/mortgage_calc_restclient/Program.java
@@ -5,7 +5,13 @@ import static java.lang.System.out;
 public class Program {
 
     public static void main(String[] args) {
-        out.println("YES");
+        String requestJsonQuery = "{ \"method_number\":1, \"loan_amount\":350000.0, \"loan_term_months\":12, \"interest_rate_percent_pa\":9.00 }";
+        String replyJsonQuery = Web.request(requestJsonQuery);
+        if (replyJsonQuery == null) {
+            out.println(" - error sending POST web query");
+            return;
+        }
+        out.println(replyJsonQuery);
     }
 
 }
---
+++ java-gradle/src/main/java/cz/petrfaltus/mortgage_calc_restclient/Web.java
@@ -0,0 +1,50 @@
+package cz.petrfaltus.mortgage_calc_restclient;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Web {
+    public static String request(String requestBody) {
+        String output = Const.EMPTY_STRING; // the default output value
+        HttpURLConnection connection = null;
+
+        try {
+            URL url = new URL(Const.URL_ADDRESS);
+
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setUseCaches(false);
+            connection.setDoOutput(true);
+            connection.addRequestProperty("User-Agent", Const.USER_AGENT);
+            connection.setRequestMethod("POST");
+
+            OutputStream os = connection.getOutputStream();
+            byte[] requestBodyBytes = requestBody.getBytes(Const.CHARSET);
+            os.write(requestBodyBytes);
+
+            InputStream is = connection.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is, Const.CHARSET);
+            BufferedReader br = new BufferedReader(isr);
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                output += line ;
+                output += System.lineSeparator();
+            }
+
+            br.close();
+        } catch (Exception e) {
+            output = null; // indicates the web request failed
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+
+        return output;
+    }
+}
