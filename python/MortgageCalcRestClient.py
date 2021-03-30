a586d18753e345e418f5c86dec013d393ad3ffa7
Petr Faltus <petrfaltus@petrfaltus.net>
2021-03-30 11:14:31 +0200

    Python - implement the web POST request for calculation

--- python/MortgageCalcRestClient.py
+++ python/MortgageCalcRestClient.py
@@ -1,2 +1,13 @@
+from classes.Web import Web
 
-print("YES")
+MESSAGE_ERROR_CONTACTING_SERVICE = "error contacting the REST service"
+
+requestJsonCalculation = "{ \"method_number\" : 1, \"loan_amount\" : 350000.0, \"loan_term_months\" : 12, \"interest_rate_percent_pa\" : 9.00 }"
+
+try:
+    replyJsonCalculation = Web.request(requestJsonCalculation)
+except:
+    print(" - " + MESSAGE_ERROR_CONTACTING_SERVICE)
+    exit()
+
+print(replyJsonCalculation)
